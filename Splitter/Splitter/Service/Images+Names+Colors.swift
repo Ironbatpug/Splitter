e0f02099d4e2dd8a056bb293dd20af94ec2ab44f
Ironbatpug <molnarcsaba92@gmail.com>
2019-03-26 20:34:29 +0100

    lesson-01 done the table

---
+++ Splitter/Splitter/Service/Images+Names+Colors.swift
@@ -0,0 +1,22 @@
+//
+//  Images+Names+Colors.swift
+//  Splitter
+//
+//  Created by Molnár Csaba on 2019. 03. 26..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import UIKit
+
+let camera = UIImage(named: "1")!
+let city = UIImage(named: "2")!
+let buffalo = UIImage(named: "3")!
+let bouqet = UIImage(named: "4")!
+let stand = UIImage(named: "5")!
+let urban = UIImage(named: "6")!
+
+let imageArray = [camera,city, buffalo, bouqet, stand, urban]
+
+let namesArray = ["CAMERA","CITY","ANIMALS","FLOWERS","STAND","URBAN"]
+
+let colorsArray = [#colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1),#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1),#colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1),#colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1),#colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1),#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)]
